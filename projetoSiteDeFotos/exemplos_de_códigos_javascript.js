02f2c9b68acde6fa0cbf4b51ae1fe499675cac32
joaopug <41847866+joaopug@users.noreply.github.com>
2025-08-28 22:29:45 -0300

    Tudo feito no primeiro semestre

---
+++ projetoSiteDeFotos/exemplos_de_códigos_javascript.js
@@ -0,0 +1,109 @@
+//O vento DOM é acionado quando todo o HTML foi completamente carregado
+document.addEventListener('DOMContentLoaded', function() {
+    //Tipos de dadoss
+    //String: Sequência de caracteres
+    //Number: números inteiros ou pontos flutuantes
+    //Boolean: retorna dois valores, true ou false
+    //object: estrutura de dados que pode armazenar múltiplos valores
+    //NULL: quando está vazio;
+    //array: objeto especial ordenado;
+    //function: bloco de código reutilizável;
+
+    //CONST É usado para declarar uma variável cujo valor é permanente
+    const teste = 50
+
+    //LET é usado para declarar variáveis que os valores podem ser retribuidos
+    let C = 5
+    C = 5
+    console.log(C)
+    
+    const notaMedia = 5;
+    
+    if (notaMedia >= 6) {        
+        //alert("passou"); // Ele será liberto 😒😒😒😒😒
+    }
+    else if (notaMedia > 4 && notaMedia < 6) {
+        //alert("foi quase mane"); // 🤢🤢🤢🤢🤢
+    }
+    else {
+        //alert("reprovado"); // O homem será degolado 😂😂😂😂😂
+    }
+
+
+    // ----------   OPERADORES ------------
+    
+
+    // = sinal de atribuição
+    // == sinal de cmparação
+    // === verifica se os dois são iguais e têm o mesmo tipo de dados (número, string)
+    // != se é diferente
+    // !== se é estritamente diferente
+    // > maior
+    // < menor
+
+    const num = 6;      
+    if (num == 6) {
+        //alert('é igual 😁');
+    }
+    else {
+        //alert("não é igual menor 😐");
+    }
+
+    const professor = "Luca";
+    if(professor == "Lucas"){
+        console.log("Sim é o Lucas")
+    } else{
+        console.log("não é o Lucas")
+    }
+
+
+// --------------------- / ------------------------
+
+// Funções são blocos de código que podem ser chamados e
+// reutilizados
+// Dentro dos parenteses da função vai os parâmetros/propriedades
+
+function jorgepica(parametro, segundoParametro) {
+    return parametro + segundoParametro;
+}
+
+const parametro = "Olá,";
+const segundoParametro = " Mundo!";
+
+console.log(jorgepica(parametro,segundoParametro));
+
+// -----------------------/-------------------------
+
+
+// Array em javascript são estruturas de dados que permitem
+// armazenar uma coleção ordenada de elementos (strings, números, objetos, arrays)
+// Arrays precisam de uma variável definida (const ou let) e também de colchetes []
+
+const primeiroArray = [ "Camiseta", "Calcinha", "Cueca", "Calça" ];
+console.log(primeiroArray[0]); // 0 é o elemento que irá aparecer o array, ou seja, camiseta 
+
+// -----------------------/--------------------------
+
+
+
+// Os objetos são coleções NÃO ORDENADAS de pares chave-valor
+// onde cada valor é associado a uma chave única
+
+
+let ser_humano = {
+    nome: "KIM",
+    idade: "18",
+    profissao: "Faz programa"
+}
+console.log(ser_humano["nome"]); // "nome" é a chave que será percorrida no objeto (vai aparecer no log)
+
+
+// ------------ / -------------
+
+const numeros = ["A", "B", "C", "D"]
+
+numeros.forEach(elemento => {
+    console.log(elemento)
+})
+
+});
---
+++ projetoSiteDeFotos/scripts.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function (){
+
+    const baseImagens = {
+        todes: ['noite_mar.jpg', 'noite_cidade.jpeg', 'onça-pintada.jpg', 'beija-flor.jpg', 'montanhas.jpg', 'campo.jpg'],
+        nature: ['montanhas.jpg', 'campo.jpg'],
+        city: ['noite_mar.jpg', 'noite_cidade.jpeg'],
+        animals: ['onça-pintada.jpg', 'beija-flor.jpg'],
+    }
+    function carregaImagens(categoria){
+        //Selecionar elementos HTML com base em um seletor
+        const galeria = document.querySelector("#galeria-imagens")
+        galeria.innerHTML = ''; //Limpar bloco, apaga tudo dentro
+
+        const imagens = baseImagens[categoria]
+
+        imagens.forEach(img => {
+            console.log(img);
+            galeria.innerHTML += "<div class='imagem-item'> <img src='imagens/"+ img +"' alt='" + img + "' /> </div>";
+        })
+    }
+    function ordenaImagens(ordem) {
+
+        const imagens = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+        
+        imagens.sort((a,b) => {
+            const nameA = a.querySelector('img').getAttribute('alt');
+            const nameB = b.querySelector('img').getAttribute('alt');
+            
+            //operador ternário - se for true ? se não :
+            return ordem == 'asc' ? nameA.localeCompare(nameB) : nameB.localeCompare(nameA); 
+
+        })
+
+        const galeria = document.getElementById('galeria-imagens');
+
+        galeria.innerHTML = '';//esvazia box das imagens
+
+        imagens.forEach(imagem => {
+            galeria.appendChild(imagem); 
+        })
+
+
+    }
+    carregaImagens('todes');
+    // Evento de clique
+    document.body.addEventListener('click', function (event){
+        if(event.target.classList.contains('botao-categoria')) {// Acessa a lista de classe de onde clicou e procura pela classe botao-categoria
+            
+            const categoria = event.target.dataset.categoria;
+
+            //alert(categoria)
+
+            carregaImagens(categoria);
+
+
+        };
+        if(event.target.classList.contains('botao-ordenar')) {
+
+            const ordem = event.target.dataset.ordem;
+            ordenaImagens(ordem);
+         }        
+    });
+})
